13fe0e23ab559bf79a4f594f2eb6c4524e15f6f5
icefox <icefox@4db09855-0bbe-6bc9-8408-d5225a1fabd9>
2011-07-14 09:11:31 +0000

    Refactoring

--- branches/3.5.x_development/core/src/main/java/net/jawr/web/config/jmx/JmxUtils.java
+++ branches/3.5.x_development/core/src/main/java/net/jawr/web/config/jmx/JmxUtils.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2009 Ibrahim Chaehoi
+ * Copyright 2009-2011 Ibrahim Chaehoi
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -13,9 +13,9 @@
  */
 package net.jawr.web.config.jmx;
 
+import static net.jawr.web.util.ServletContextUtils.getContextPath;
+
 import java.lang.management.ManagementFactory;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Properties;
 
 import javax.management.MBeanServer;
@@ -26,6 +26,7 @@ import javax.servlet.ServletContext;
 import net.jawr.web.JawrConstant;
 import net.jawr.web.exception.JmxConfigException;
 import net.jawr.web.servlet.JawrRequestHandler;
+import net.jawr.web.util.ServletContextUtils;
 import net.jawr.web.util.StringUtils;
 
 import org.apache.log4j.Logger;
@@ -39,18 +40,6 @@ public final class JmxUtils {
 
 	/** The logger */
 	private static final Logger LOGGER = Logger.getLogger(JmxUtils.class);
-
-	/** The context path parameter */
-	private static final String CONTEXT_PATH_PARAM_NAME = "contextPath";
-	
-	/** The default context path used for the application if no context oath is defined */
-	private static final String DEFAULT_CONTEXT_PATH_NAME = "default";
-
-	/** The default context path used for the application if no context oath is defined */
-	private static final String DEFAULT_CONTEXT_PATH = "/"+DEFAULT_CONTEXT_PATH_NAME;
-
-	/** The getContextPath method name to retrieve the context path for servlet API 2.5 and above */
-	private static final String GET_CONTEXT_PATH_METHOD = "getContextPath";
 
 	/** The property which enables the use of JMX */
 	public static final String JMX_ENABLE_FLAG_SYSTEM_PROPERTY = "com.sun.management.jmxremote";
@@ -148,61 +137,6 @@ public final class JmxUtils {
 	}
 
 	/**
-	 * Returns the context path associated to the servlet context
-	 * @param servletContext the servlet context
-	 * @return the context path associated to the servlet context
-	 * @throws Exception if an exception occurs
-	 */
-	public static String getContextPath(ServletContext servletContext) {
-		String contextPath = null;
-		
-		// Get the context path
-		
-		// If the servlet API version is greater or equals to 2.5, use the getContextPath method
-		if(servletContext.getMajorVersion() > 2 || servletContext.getMajorVersion() == 2 && servletContext.getMinorVersion() >= 5){
-			
-			contextPath = getContextPathForNewServletApi(servletContext);
-			
-		}else{ // Retrieve the context path from the init parameter or the servlet context
-			contextPath = servletContext.getInitParameter(CONTEXT_PATH_PARAM_NAME);
-		}
-		
-		if(StringUtils.isEmpty(contextPath)){
-			LOGGER.warn("No context path defined for this web application. You will face issues, if you are deploying mutiple web app, without defining the context.\n" +
-					"If you are using a server with Servlet API less than 2.5, please use the context parameter 'contextPath' in your web.xml to define the context path of the application.");
-			
-			contextPath = DEFAULT_CONTEXT_PATH;
-		}
-		
-		return contextPath;
-	}
-
-	/**
-	 * Returns the context path using the servlet API version 2.5 method
-	 * @param servletContext the servlet context
-	 * @return the context path
-	 */
-	private static String getContextPathForNewServletApi(
-			ServletContext servletContext) {
-		String contextPath = null;
-		try {
-			Method getServletContextPathMethod = servletContext.getClass().getMethod(GET_CONTEXT_PATH_METHOD, new Class[] {});
-			contextPath = (String) getServletContextPathMethod.invoke(servletContext, (Object[])null);
-		} catch (SecurityException e) {
-			throw new JmxConfigException(e);
-		} catch (NoSuchMethodException e) {
-			throw new JmxConfigException(e);
-		} catch (IllegalArgumentException e) {
-			throw new JmxConfigException(e);
-		} catch (IllegalAccessException e) {
-			throw new JmxConfigException(e);
-		} catch (InvocationTargetException e) {
-			throw new JmxConfigException(e);
-		}
-		return contextPath;
-	}
-
-	/**
 	 * Returns the object name for the Jawr configuration Manager MBean
 	 * @param contextPath the context path
 	 * @param resourceType  the resource type
@@ -213,10 +147,8 @@ public final class JmxUtils {
 		
 		String curCtxPath = contextPath;
 		if(StringUtils.isEmpty(curCtxPath)){
-			LOGGER.warn("No context path defined for this web application. You will face issues, if you are deploying mutiple web app, without defining the context.\n" +
-				"If you are using a server with Servlet API less than 2.5, please use the context parameter 'contextPath' in your web.xml to define your context path of the application.");
-	
-			curCtxPath = DEFAULT_CONTEXT_PATH_NAME;
+			
+			curCtxPath = ServletContextUtils.getContextPath(null);
 		}
 		
 		if(curCtxPath.charAt(0) == ('/')){
