affb6e13b9fa8b8dc4447700e0f550a7b1af3ad3
icefox <icefox@4db09855-0bbe-6bc9-8408-d5225a1fabd9>
2011-08-06 10:56:23 +0000

    Issue #203 : Issue with generator when using JRockit R27.4.0-xxx

--- core/src/main/java/net/jawr/web/resource/bundle/generator/ResourceGeneratorReaderProxyFactory.java
+++ core/src/main/java/net/jawr/web/resource/bundle/generator/ResourceGeneratorReaderProxyFactory.java
@@ -185,12 +185,39 @@ public class ResourceGeneratorReaderProxyFactory {
 			boolean result = false;
 			
 			for (int i = 0; i < methods.length && !result; i++) {
-				if(methods[i].equals(m)){
+				if(methodEquals (methods [i], m)){
 					result = true;
 				}
 			}
 			
 			return result;
+		}
+		
+		/**
+		 * Checks if the 2 methods are equals.
+		 * 
+		 * @param method the first method
+		 * @param other the second method
+		 * @return true if the 2 methods are equals
+		 */
+		private boolean methodEquals( Method method, Method other){
+		    
+			if ((method.getDeclaringClass().equals( other.getDeclaringClass()))
+		    && (method.getName().equals( other.getName()))) {
+		        if (!method.getReturnType().equals(other.getReturnType()))
+		            return false;
+		        
+		        Class<?>[] params1 = method.getParameterTypes();
+		        Class<?>[] params2 = other.getParameterTypes();
+		        if (params1.length == params2.length) {
+		            for (int i = 0; i < params1.length; i++) {
+		            if (params1[i] != params2[i])
+		                return false;
+		            }
+		            return true;
+		        }
+		    }
+		    return false;
 		}
 	}
 	
